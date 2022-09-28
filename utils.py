949b47a2a931d997c6f0c3764ec47a724c8d46fe
ku60 <111289102+ku60@users.noreply.github.com>
2022-09-28 14:54:48 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -132,33 +132,33 @@ def get_account_name(row_num, col_num, sheet):
 	count = 0
 
 	for i in range(1,following_num+1):
-	if count >= 10:
-		driver.execute_script("window.scrollBy(0, 90);")
-		if count%10 == 5:
-			time.sleep(5)
-		try:
-			account = driver.find_element("xpath", "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[11]/div/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span")
-			account_name = account.text.replace("@","")
-			row = next_available_row(sheet, col_num)
-			cell = chr(col_num+96) + row
-			sheet.update_acell(cell, account_name)
-			if int(sheet.cell(int(row), 2).value)>=2 :
-				sheet.delete_row(int(row)) 
-		except:
-			pass
+		if count >= 10:
+			driver.execute_script("window.scrollBy(0, 90);")
+			if count%10 == 5:
+				time.sleep(5)
+			try:
+				account = driver.find_element("xpath", "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[11]/div/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span")
+				account_name = account.text.replace("@","")
+				row = next_available_row(sheet, col_num)
+				cell = chr(col_num+96) + row
+				sheet.update_acell(cell, account_name)
+				if int(sheet.cell(int(row), 2).value)>=2 :
+					sheet.delete_row(int(row)) 
+			except:
+				pass
+			count += 1
+		else:
+			try:
+				account = driver.find_element("xpath", f"/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[{i}]/div/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span")
+				account_name = account.text.replace("@","")
+				row = next_available_row(sheet, col_num)
+				cell = chr(col_num+96) + row
+				sheet.update_acell(cell, account_name)
+				if int(sheet.cell(int(row), 2).value)>=2 :
+					sheet.delete_row(int(row)) 
+			except:
+				pass
 		count += 1
-	else:
-		try:
-			account = driver.find_element("xpath", f"/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[{i}]/div/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span")
-			account_name = account.text.replace("@","")
-			row = next_available_row(sheet, col_num)
-			cell = chr(col_num+96) + row
-			sheet.update_acell(cell, account_name)
-			if int(sheet.cell(int(row), 2).value)>=2 :
-				sheet.delete_row(int(row)) 
-		except:
-			pass
-	count += 1
 
 
 """
